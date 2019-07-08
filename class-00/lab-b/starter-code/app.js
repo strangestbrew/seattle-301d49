eb9e6dd8da78cb38948b64ac51e9518c2ea104c2
strangestbrew <thomas.lyndsey@gmail.com>
2019-07-08 11:33:40 -0700

    wrapping up lab00b

--- class-00/lab-b/starter-code/app.js
+++ class-00/lab-b/starter-code/app.js
@@ -108,62 +108,37 @@ console.log('Hello from the new object function', newObject(['hi', 'hello', 'are
 
 const sum = (a, b, c, d) => {
   return (a, b, c, d);
-}
-
-// TODO: Uncomment the following line of code to see the output in the browser console
+};
 console.log(sum(1, 2, 3, 4));
 
-const  = array => ({
-  firstValue: array[0],
-  secondValue: array[1],
-  thirdValue: array[2]
+const objectLit = () => ({
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value3',
 });
-
-
-let objectLit = function() {
-  return {
-    key1: 'value1',
-    key2: 'value2',
-    key3: 'value3',
-  };
-};
-
-// TODO: Uncomment the following line of code to see the output in the browser console
 console.log(objectLit());
 
-
-let sumAndProduct = function(a, b) {
+const sumAndProduct = (a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
 };
+console.log(sumAndProduct(3, 9));
 
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
-
-
-let message = function(name) {
-  return `Hello, ${name}!`;
-};
-
-// TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
-
+const message = (name) => `Hello, ${name}!`;
+console.log(message('Allie'));
 
 let Student = function(name, age, hometown) {
   this.name = name;
   this.age = age;
   this.hometown = hometown;
 };
-
 let joe = new Student('Joe', 'Schmoe', 100);
-
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
-
+console.log(joe);
 // TODO: After viewing the previous console.log(), return the code to a working state.
-
+//cannot be changed to an arrow function, as it is scoped and has changing "this" parameters
 
 
 Student.prototype.greeting = function() {
@@ -172,23 +147,20 @@ Student.prototype.greeting = function() {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
-
+console.log(joe.greeting());
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
-
-Student.courseName = function() {
+Student.courseName = () =>{
   return 'This student is enrolled in Code 301.';
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
-
-
+console.log(Student.courseName());
 
 // STEP 11
 // How do arrow functions affect constructor functions?
+//
 Student.prototype.scope = function() {
   console.log(this);
 };
